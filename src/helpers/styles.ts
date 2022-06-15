dff9e7bf8fbdd7c7a08007b36af79c17b7f9a182
Mauro Falcone <maurofalcone.95@gmail.com>
2022-06-15 09:50:28 -0300

    handle mobile edge cases

--- src/helpers/styles.ts
+++ src/helpers/styles.ts
@@ -1,6 +1,8 @@
 export enum MediaQueries {
-  XS = "(min-width: 0px)",
-  SM = "(min-width: 600px)",
+  NONE = "(min-width: 280px)",
+  XXS = "(min-width: 360px)",
+  XS = "(min-width: 415px)",
+  SM = "(min-width: 600px",
   MD = "(min-width: 900px)",
   LG = "(min-width: 1200px)",
   XL = "(min-width: 1536px)",
